d6af2a9ffbcec1bdc64651f02c866d5de4e76724
taras131 <mossnabitkana@gmail.com>
2020-10-07 00:19:04 +0300

    modal1.3

--- js/script.js
+++ js/script.js
@@ -123,14 +123,19 @@
             statusMessage.classList.add(`status`);
             statusMessage.textContent = message.loading;
             form.appendChild(statusMessage);
-                      
+                                
             const request = new XMLHttpRequest();
-            request.open(`POST`,`server.php`);
- //           request.setRequestHeader(`Content-type`,`multipart/form-date`);
-
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
             const formData = new FormData(form);
-            request.send(formData);
 
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+
+            request.send(json);
             request.addEventListener(`load`,()=>{
                 if(request.status === 200){
                     console.log(request.response);
